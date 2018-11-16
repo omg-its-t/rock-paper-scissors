cb56e66bef53288d67ba163d806431cc7ae1dd4d
Theo Mellske <blackvq35@gmail.com>
2018-11-15 19:46:31 -0600

    game logic done, potential to refactor

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
@@ -25,12 +25,58 @@ function main(){
 }
 
 function game(userChoice){
-  var computerChoice = getComputerChoice();
-  console.log(computerChoice + " " + userChoice);
+  const computerChoice = getComputerChoice();
+  switch(userChoice + computerChoice){
+    case "rs":
+    case "pr":
+    case "sp":
+      console.log("user win");
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      console.log("user looses");
+      loose(userChoice, computerChoice);
+        break;
+    case "rr":
+    case "pp":
+    case "ss":
+        console.log("its a draw");
+        draw(userChoice, computerChoice);
+          break;
+  }
 }
 
 function getComputerChoice(){
   const choices = ["r", "p", "s"];
   //generate random num between 0-2 use as array index and return
   return choices[Math.floor(Math.random() * 3)];
+}
+
+function win(user, computer){
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  result_div.innerHTML = `${getFullWord(user)} beats ${getFullWord(computer)} you WIN!`;
+}
+
+function loose(user, computer){
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = `${getFullWord(computer)} beats ${getFullWord(user)} you LOOSE!`;
+}
+
+function draw(user, computer){
+result_div.innerHTML = `${getFullWord(user)} AND ${getFullWord(computer)} is a DRAW!`;
+}
+
+function getFullWord(letter){
+  if(letter === 'r'){
+    return "Rock";
+  }
+  if(letter === 'p'){
+    return "Paper";
+  }else{
+    return "Scissors";
+  }
 }
